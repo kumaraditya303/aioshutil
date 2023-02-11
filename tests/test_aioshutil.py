0adb8d2a38813b6d22854d38fd449425d995105c
DontPanicO <dontpanic.call0@gmail.com>
2023-02-11 23:32:10 +0000

    switched to `math.isclose` to compare file timestamps
    access and/or modification times may be truncated when copying files or stats

--- tests/test_aioshutil.py
+++ tests/test_aioshutil.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 import os
 import stat
 from pathlib import Path
@@ -36,9 +37,10 @@ async def test_copystat(tmp_path: Path):
     tmp_path.joinpath("temp1.txt").write_text("Hello World!", encoding="utf-8")
     tmp_path.joinpath("temp1.txt").chmod(stat.S_IEXEC)
     await aioshutil.copystat(tmp_path / "temp.txt", tmp_path / "temp1.txt")
-    assert (
-        tmp_path.joinpath("temp1.txt").stat().st_atime
-        == tmp_path.joinpath("temp.txt").stat().st_atime
+    assert math.isclose(
+        tmp_path.joinpath("temp1.txt").stat().st_atime,
+        tmp_path.joinpath("temp.txt").stat().st_atime,
+        rel_tol=1e-6,
     )
     assert (
         tmp_path.joinpath("temp1.txt").stat().st_mode
@@ -64,9 +66,10 @@ async def test_copy2(tmp_path: Path):
         tmp_path.joinpath("temp.txt").stat().st_mode | stat.S_IEXEC
     )
     await aioshutil.copy2(tmp_path.joinpath("temp.txt"), tmp_path.joinpath("temp1.txt"))
-    assert (
-        tmp_path.joinpath("temp1.txt").stat().st_atime
-        == tmp_path.joinpath("temp.txt").stat().st_atime
+    assert math.isclose(
+        tmp_path.joinpath("temp1.txt").stat().st_atime,
+        tmp_path.joinpath("temp.txt").stat().st_atime,
+        rel_tol=1e-6,
     )
     assert (
         tmp_path.joinpath("temp1.txt").stat().st_mode
